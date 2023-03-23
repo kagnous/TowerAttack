e638f53cc5aa1223949fb43fee402aa8e7753f34
kagnous <antoinecasa34@gmail.com>
2023-03-23 16:08:21 +0100

    Add Feature & Debug
    Wave system upraded, no fireshoot ect.

--- Assets/_Scripts/GamelayElementScript/StructureRuins.cs
+++ Assets/_Scripts/GamelayElementScript/StructureRuins.cs
@@ -17,37 +17,7 @@ public class StructureRuins : Entity
 
     [Tooltip("Event lancé à la construction du batiment")]
     public UnityEvent spawnStructure;
-
-    [SerializeField]
-    private GameObject mur1;
-    [SerializeField]
-    private GameObject mur2;
-    [SerializeField]
-    private GameObject mur3;
-    [SerializeField]
-    private GameObject mur4;
-    [SerializeField]
-    private GameObject mur5;
-    [SerializeField]
-    private GameObject mur6;
-    [SerializeField]
-    private GameObject mur7;
-    [SerializeField]
-    private Material wallsAllies;
-    [SerializeField]
-    private Material wallsEnnemies;
-    // Trigger pour changer la couleur des murs pour le start 
-    public void Start()
-    {
-        mur1.GetComponent<MeshRenderer> ().material = wallsEnnemies;
-        mur2.GetComponent<MeshRenderer> ().material = wallsEnnemies;
-        mur3.GetComponent<MeshRenderer> ().material = wallsEnnemies;
-        mur4.GetComponent<MeshRenderer> ().material = wallsEnnemies;
-        mur5.GetComponent<MeshRenderer> ().material = wallsEnnemies;
-        mur6.GetComponent<MeshRenderer> ().material = wallsEnnemies;
-        mur7.GetComponent<MeshRenderer> ().material = wallsEnnemies;
-    }
-
+    
     public override void OnClick()
 
     {
@@ -65,13 +35,6 @@ public class StructureRuins : Entity
         {
             structure = Instantiate(structurePrefab, new Vector3(transform.position.x, transform.position.y +1.2f, transform.position.z), transform.rotation);
             spawnStructure?.Invoke();
-            mur1.GetComponent<MeshRenderer> ().material = wallsAllies;
-            mur2.GetComponent<MeshRenderer> ().material = wallsAllies;
-            mur3.GetComponent<MeshRenderer> ().material = wallsAllies;
-            mur4.GetComponent<MeshRenderer> ().material = wallsAllies;
-            mur5.GetComponent<MeshRenderer> ().material = wallsAllies;
-            mur6.GetComponent<MeshRenderer> ().material = wallsAllies;
-            mur7.GetComponent<MeshRenderer> ().material = wallsAllies;
         }
     }
 }
